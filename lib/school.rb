dcc15cd9c4958419ad7a33e4d68a4f1d0d08c7cf
Joel Kelly <joel.kelly.philly@gmail.com>
2020-03-30 15:29:58 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -7,7 +7,11 @@ class School
   end
 
   def add_student(student_name, grade)
-    
+    if @roster.has_key?(grade)
+      @roster[grade] << student_name
+    else
+      @roster[grade] = []
+      @roster[grade] << student_name
   end
 
 end
